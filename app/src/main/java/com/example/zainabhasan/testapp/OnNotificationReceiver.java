146a47f20333b4e5b85dd20abc0f045a78cd332a
ZAINAB-HASAN <zanies.hasan.info02@gmail.com>
2018-10-17 12:29:37 +0530

    fcm-notif

---
+++ app/src/main/java/com/example/zainabhasan/testapp/OnNotificationReceiver.java
@@ -0,0 +1,12 @@
+package com.example.zainabhasan.testapp;
+
+import android.content.Intent;
+
+/**
+ * Created by Administrator on 3/12/2018.
+ */
+
+public interface OnNotificationReceiver {
+
+    void onFCMReceiver(Intent intent);
+}
